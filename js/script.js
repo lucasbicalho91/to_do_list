173918a0c4c6f6fe5e66c904787f924610c491fa
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-09-26 17:39:33 -0300

    javascript logic done

--- js/script.js
+++ js/script.js
@@ -24,7 +24,41 @@ function addItem(e) {
   input_item.type = 'text';
   input_item.value = item_value;
   input_item.setAttribute('readonly', 'readonly');
+  input_item.addEventListener('dblclick', function(){
+    input_item.style.textDecoration = "line-through";
+  })
   item_content.appendChild(input_item);
 
+  const item_action = document.createElement('div');
+  item_action.classList.add('actions');
+
+  const edit_item = document.createElement('button');
+  edit_item.classList.add('edit', 'btn', 'btn-success');
+  edit_item.type="button";
+  edit_item.innerText = 'Edit';
+
+  const delete_item = document.createElement('button');
+  delete_item.classList.add('delete','btn','btn-danger','fa','fa-trash');
+
+  item_action.appendChild(edit_item);
+  item_action.appendChild(delete_item);
+
+  item.appendChild(item_action);
+
   taskContainer.appendChild(item);
+
+  edit_item.addEventListener('click', (e) => {
+    if (edit_item.innerText.toLowerCase() == 'edit') {
+      edit_item.innerText = "Save";
+      input_item.removeAttribute('readonly');
+      input_item.focus();
+    } else {
+      edit_item.innerText = "Edit";
+      input_item.setAttribute("readonly", "readonly")
+    }
+  });
+
+  delete_item.addEventListener('click', (e) => {
+    taskContainer.removeChild(item);
+  });
 }
